be6ef8e5485687215c49d6912320f1a336b64e49
Shubham Singh <164128999+Shubhambth@users.noreply.github.com>
2024-10-06 10:11:29 +0000

    final cummit

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,9 @@ from .import views
 urlpatterns = [
     path('',views.index,name="home"),
     path('upload/',views.upload_wallpaper,name='upload'),
-    path('mupload/',views.mobile_wallpaper,name='mupload')
+    path('mupload/',views.mobile_wallpaper,name='mupload'),
+    path('login/',views.login_user,name='login'),
+    
     
     
 ]
--- main/views.py
+++ main/views.py
@@ -1,19 +1,38 @@
 from django.shortcuts import render, redirect
 from .forms import WallpaperForm , Mobileform
 from .models import Wallpaper , MobileWallpaper
+from django.contrib.auth import login , authenticate
+from django.contrib.auth.decorators import login_required
+
+
+
+
+
 
 
 def index(request):
-  wallpapers = Wallpaper.objects.all().order_by('-upload_date')
-  mwallpaper = MobileWallpaper.objects.all().order_by('-upload_date')
-  return render(request, 'index.html',{'wallpapers': wallpapers , 
+    wallpapers = Wallpaper.objects.all().order_by('-upload_date')
+    mwallpaper = MobileWallpaper.objects.all().order_by('-upload_date')
+    
+
+    if request.method == 'GET':
+        search_term = request.GET.get('search_term')
+        if search_term:
+            wallpapers = wallpapers.filter(title__icontains=search_term)
+            mwallpaper = mwallpaper.filter(title__icontains=search_term)
+            return render(request, 'index.html', {'wallpapers': wallpapers , 
+                                'mwallpaper': mwallpaper})
+    
+    
+            
+    return render(request, 'index.html',{'wallpapers': wallpapers , 
                                        'mwallpaper': mwallpaper})
 
 
 # View for uploading wallpapers
 
 
-
+@login_required(login_url="login")
 def upload_wallpaper(request):
   if request.method == 'POST':
       form = WallpaperForm(request.POST, request.FILES )
@@ -26,7 +45,7 @@ def upload_wallpaper(request):
   return render(request, 'upload.html', {'form': form})
 
 
-
+@login_required(login_url="login")
 def mobile_wallpaper(request):
   if request.method == 'POST':
       form = Mobileform(request.POST, request.FILES)
@@ -40,6 +59,15 @@ def mobile_wallpaper(request):
 
 
 
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    return render(request,'login.html')        
 
 
 # Create your views here.
